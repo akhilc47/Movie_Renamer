99265ff573596f16cea1fdf865c0e88d3833a5fa
akhilc47 <akhilc1990@gmail.com>
2018-07-06 22:57:45 +0530

    Moved get_original_title to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+from moviedb import get_response
+
 '''
 Keep all helper functions needed for renamer.py here.
 '''
@@ -6,6 +8,8 @@ Keep all helper functions needed for renamer.py here.
 def preproc_title(bad_title: str="Titanic")-> list:
     '''
     Cleans up the title and extract year if any.
+
+    Returns list with tokens extracted plus year(=0 default).
     '''
     bad_title = bad_title.replace('.', ' ')
     bad_title = bad_title.replace('(', ' ').replace(')', ' ')
@@ -20,3 +24,36 @@ def preproc_title(bad_title: str="Titanic")-> list:
         title_and_year.append(token)
     title_and_year.append(year)
     return title_and_year
+
+
+def get_original_title(old_title: list=["Titanic"], year: int=1997):
+    '''
+    Algorithm for using the tokens with year to find the correct title.
+
+    Returns an empty string if no match was found.
+    '''
+    original_title = ""
+    for i in range(len(old_title)):
+        query_title = ' '.join(old_title[i:])
+        web_response = get_response(query_title)
+        if web_response['total_results'] == 0:
+            print(query_title, "0 results")
+            continue
+        print(query_title, "Looping through results")
+        for movie in web_response['results']:
+            if not (year and movie['release_date']):
+                continue
+            elif year != int(movie['release_date'][0:4]):
+                continue
+            print("YEAR MATCHED,", "query:", year, "response:",
+                  movie['release_date'])
+            if movie['title'] == query_title.strip():
+                print("TITLE MATCHED,", "query:", query_title,
+                      "response:", movie['title'])
+                original_title = movie['title']
+                break
+        if original_title:
+            break
+        if web_response['total_results'] > 20:
+            print("Too many results and nothing matches on first page")
+    return original_title
--- renamer.py
+++ renamer.py
@@ -1,7 +1,6 @@
 import pprint
 import os
-from moviedb import get_response
-from helpers import preproc_title
+from helpers import preproc_title, get_original_title
 
 path = "E:/Downloads/malayalam"
 fout = open('output.csv', 'w')
@@ -9,33 +8,9 @@ for item in os.listdir(path):
     title_with_year = preproc_title(item)
     old_title = title_with_year[:-1]
     year = title_with_year[-1]
-    original_title = ""
-    for i in range(len(old_title)):
-        query_title = ' '.join(old_title[i:])
-        web_response = get_response(query_title)
-        if web_response['total_results'] == 0:
-            print(query_title, "0 results")
-            continue
-        print(query_title, "Looping through results")
-        for movie in web_response['results']:
-            if not (year and movie['release_date']):
-                continue
-            elif year != int(movie['release_date'][0:4]):
-                continue
-            print("YEAR MATCHED,", "query:", year, "response:",
-                  movie['release_date'])
-            if movie['title'] == query_title.strip():
-                print("TITLE MATCHED,", "query:", query_title,
-                      "response:", movie['title'])
-                original_title = movie['title']
-                break
-        if original_title:
-            break
-        if web_response['total_results'] > 20:
-            print("Too many results and nothing matches on first page")
+    original_title = get_original_title(old_title, year)
     if original_title:
         fout.write(item+","+original_title+"\n")
     else:
         fout.write(item+", couldn't find original title"+"\n")
-    print("*"*30)
 fout.close()
