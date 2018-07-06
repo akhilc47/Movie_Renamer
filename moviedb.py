7f339d69b3bd1b4f4503cabb1ecc2cb50da23739
akhilc47 <akhilc1990@gmail.com>
2018-07-06 10:16:01 +0530

    version 0.1

--- moviedb.py
+++ moviedb.py
@@ -6,16 +6,19 @@ with open('config.txt') as keyh:
     api_key = keyh.read().strip()
 
 
-def get_response(movie_title: str='Titanic') -> dict:
+def get_response(movie_title: str='Titanic', page_num: int=1) -> dict:
     '''
     This function returns the response from tmdb for a given movie title.
 
     Input must be a string type movie title.
+
+    If more than 20 results, page number can be used to access next pages.
 
     Output is a dictionary with the result(s).
     '''
     tmdb_url = 'https://api.themoviedb.org/3/search/movie?api_key='
     movie_title = urllib.parse.quote(movie_title) # Escape non-ascii titles
     tmdb_query = '&query=' + '+'.join(movie_title.split())
-    response = urllib.request.urlopen(tmdb_url+api_key+tmdb_query).read()
+    page = '&page='+str(page_num)
+    response = urllib.request.urlopen(tmdb_url+api_key+tmdb_query+page).read()
     return json.loads(response)
--- renamer.py
+++ renamer.py
@@ -1,62 +1,41 @@
 import pprint
 import os
 from moviedb import get_response
+from helpers import preproc_title
 
-path = "E:/Downloads"
-for i, item in enumerate(os.listdir(path)):
-    if os.path.isdir(os.path.join(path, item)):
-        print (i, item)
-    bad_title = item.replace('.', ' ')
-    bad_title = bad_title.replace('(', ' ').replace(')', ' ')
-    bad_title = bad_title.replace('[', ' ').replace(']', ' ')
-    bad_title = bad_title.split()
-    print(bad_title)
-    year = 0
-    new_title_list = []
-    for token in bad_title:
-        if token.isdigit() and (int(token) > 1900 and int(token) < 2020):
-            print("year of release: ", token)
-            year = int(token)
-            break
-        new_title_list.append(token)
-    if year:
-        print(new_title_list)
-        query_title = ""
-        original_title = ""
-        for token in reversed(new_title_list):
-            query_title = token + " " + query_title
-            print(query_title)
-            js_response = get_response(query_title)
-            #pprint.pprint(js_response)
-            if js_response['total_results'] == 0:
-                print ("Could not find correct title")
-            elif js_response['total_results'] == 1:
-                if int(js_response['results'][0]['release_date'][0:4]) == year:
-                    print ("Found the original title")
-                    original_title = js_response['results'][0]['title']
-                    break
-            elif js_response['total_results'] > 20:
+path = "E:/Downloads/malayalam"
+fout = open('output.csv', 'w')
+for item in os.listdir(path):
+    title_with_year = preproc_title(item)
+    old_title = title_with_year[:-1]
+    year = title_with_year[-1]
+    original_title = ""
+    for i in range(len(old_title)):
+        query_title = ' '.join(old_title[i:])
+        web_response = get_response(query_title)
+        if web_response['total_results'] == 0:
+            print(query_title, "0 results")
+            continue
+        print(query_title, "Looping through results")
+        for movie in web_response['results']:
+            if not (year and movie['release_date']):
                 continue
-            else:
-                for result in js_response['results']:
-                    if not result['release_date']:
-                        continue
-                    elif year != int(result['release_date'][0:4]):
-                        continue
-                    else:
-                        print("Found matching year")
-                        pprint.pprint(result)
-                    print (result['title'][-len(query_title):], query_title)
-                    if result['title'][-len(query_title):] == query_title.strip():
-                        print("Found partially matching title")
-                        if original_title == "":
-                            original_title = result['title']
-                        else:
-                            print("Multiple matches, add more token to query")
-                            break
-                        print(original_title)
-                if original_title:
-                    break
+            elif year != int(movie['release_date'][0:4]):
+                continue
+            print("YEAR MATCHED,", "query:", year, "response:",
+                  movie['release_date'])
+            if movie['title'] == query_title.strip():
+                print("TITLE MATCHED,", "query:", query_title,
+                      "response:", movie['title'])
+                original_title = movie['title']
+                break
+        if original_title:
+            break
+        if web_response['total_results'] > 20:
+            print("Too many results and nothing matches on first page")
     if original_title:
-        print ("Found original title:", original_title)
-    print ("*"*30)
+        fout.write(item+","+original_title+"\n")
+    else:
+        fout.write(item+", couldn't find original title"+"\n")
+    print("*"*30)
+fout.close()
