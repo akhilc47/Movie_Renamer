bda4aac6dc909abeacc3ab772e8d684442a51675
turner-prize <hiphoperation@hotmail.com>
2018-07-07 22:17:47 +0100

    Update helpers.py
    added import re for regular expression usage as per comment (https://www.reddit.com/r/learnpython/comments/8wuciq/feedback_for_my_beginner_project_in_github/e1ysutc/) on reddit. changed the multiple lines of replacing different characters withing bad_title to one regex sub line which can now be added to if more special characters appear which need removing.

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 from moviedb import get_response
+import re
 
 '''
 Keep all helper functions needed for renamer.py here.
@@ -11,10 +12,7 @@ def preproc_title(bad_title: str="Titanic")-> list:
 
     Returns list with tokens extracted plus year(=0 default).
     '''
-    bad_title = bad_title.replace('.', ' ')
-    bad_title = bad_title.replace('(', ' ').replace(')', ' ')
-    bad_title = bad_title.replace('[', ' ').replace(']', ' ')
-    bad_title = bad_title.split()
+    bad_title = re.sub((r'[[|\]|,|.]',' ',bad_title)
     year = 0
     title_and_year = []
     for token in bad_title:
